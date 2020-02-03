f38a740ad50a2496143b3860dec3d982a175015b
Jack Reed <phillipjreed@gmail.com>
2020-02-03 09:37:01 -0800

    Add slack signup

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,4 +1,21 @@
-exports.handler = async event => {
-  const email = JSON.parse(event.body).payload.email
-  console.log(`Recieved a submission: ${email}`)
+import axios from 'axios';
+
+export async function handler(event, context, callback){
+  const email = JSON.parse(event.body).payload.email.trim();
+  console.log(`Recieved a submission: ${email}`);
+  const SLACK_TOKEN = process.env.SLACK_TOKEN;
+  const SLACK_INVITE_ENDPOINT = 'https://slack.com/api/users.admin.invite';
+  const toSlack = `email=${email}&token=${SLACK_TOKEN}&set_active=true`;
+  await axios.get(`${SLACK_INVITE_ENDPOINT}?${toSlack}`)
+  .then((response) => {
+    console.log(`Successfully invited ${email}`);
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'success'
+      })
+    });
+  }).catch((error) => {
+    console.log(error);
+  })
 }
